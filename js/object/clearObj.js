ab2de852bd535419d570118990cfac6b8f51a3c2
gefangshuai <gefangshuai@163.com>
2020-07-13 16:51:53 +0800

    coding

--- js/object/clearObj.js
+++ js/object/clearObj.js
@@ -1,7 +1,7 @@
 const clearObj = obj => {
     let target = {};
     Object.keys(obj).forEach(key => {
-        if (obj[key])
+        if (obj[key] !== undefined && obj[key] !== '' && obj[key] !== null)
             target[key] = obj[key]
     });
     return target;
