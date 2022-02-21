abb3b4a86fed0ac33f7c4621009a5a123aee035f
Timothy.Ge <gefangshuai@163.com>
2022-02-21 15:31:05 +0800

    coding

--- js/util/appendPath.js
+++ js/util/appendPath.js
@@ -3,13 +3,15 @@
  * @returns {string}
  */
 const appendPath = (prefix, path) => {
-    if (prefix.endsWith("/")) {
-        prefix = prefix.slice(0, -1)
+    if (prefix && path) {
+        if (prefix.endsWith("/")) {
+            prefix = prefix.slice(0, -1)
+        }
+        if (path.startsWith('/')) {
+            path = path.slice(1)
+        }
+        return prefix + '/' + path
     }
-    if (path.startsWith('/')) {
-        path = path.slice(1)
-    }
-    return prefix + '/' + path
 }
 
 export default appendPath
